4cbbc2444ce22b0710184b66fc8aae8568e8d45c
M@4rk$ <masterchef_101@outlook.de>
2023-01-18 15:07:25 +0100

    Init Project

---
+++ src/main/java/de/struma/SuperDuperMarkt/SuperDuperMarktApplication.java
@@ -0,0 +1,13 @@
+package de.struma.SuperDuperMarkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDuperMarktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDuperMarktApplication.class, args);
+	}
+
+}
