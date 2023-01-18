3af24d62cc210da184a215264327469d1db20bec
M@4rk$ <masterchef_101@outlook.de>
2023-01-18 22:38:21 +0100

    template import mit controller angelegt

---
+++ src/main/java/de/struma/SuperDuperMarkt/controller/ImportController.java
@@ -0,0 +1,20 @@
+package de.struma.SuperDuperMarkt.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping(value = {"/import"})
+@Controller
+public class ImportController {
+
+	@GetMapping
+	public String showHome(Model model) {
+		return "pages/import";
+	}
+	
+
+}
