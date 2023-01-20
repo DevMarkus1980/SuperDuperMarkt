dbb4946198080bf66583419d8a2c77b9abeff98a
M@4rk$ <masterchef_101@outlook.de>
2023-01-20 16:16:07 +0100

    inventoryController bereinigt

--- src/main/java/de/struma/SuperDuperMarkt/controller/InventoryController.java
+++ src/main/java/de/struma/SuperDuperMarkt/controller/InventoryController.java
@@ -1,11 +1,15 @@
 package de.struma.SuperDuperMarkt.controller;
 
+import de.struma.SuperDuperMarkt.model.Artikel;
 import de.struma.SuperDuperMarkt.service.ArtikelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @RequestMapping(value = {"/inventory"})
@@ -20,10 +24,7 @@ public class InventoryController {
 
 	@GetMapping
 	public String showInventory(Model model) {
-
-		artikelService.setDummyList();
 		model.addAttribute("artikelList", artikelService.getArtikelList());
-
 		return "pages/inventory";
 	}
 	
