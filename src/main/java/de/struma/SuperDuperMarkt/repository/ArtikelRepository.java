0fe482db7819f012d579c8cf625d89574f142105
M@4rk$ <masterchef_101@outlook.de>
2023-01-22 13:14:34 +0100

    ArtikelREpository um eine Methode erweitert

--- src/main/java/de/struma/SuperDuperMarkt/repository/ArtikelRepository.java
+++ src/main/java/de/struma/SuperDuperMarkt/repository/ArtikelRepository.java
@@ -3,6 +3,10 @@ package de.struma.SuperDuperMarkt.repository;
 import de.struma.SuperDuperMarkt.model.Artikel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArtikelRepository extends JpaRepository<Artikel, Long> {
+
+    List<Artikel> findAllByRegalAuslegen(Boolean eingeraumtOderNicht);
 
 }
