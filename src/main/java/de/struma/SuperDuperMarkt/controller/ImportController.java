71297ac185ecac62153b6837884ad6a2270912da
M@4rk$ <masterchef_101@outlook.de>
2023-01-25 12:30:15 +0100

    Importer im Frontend mit implementiert und controller erweitert

--- src/main/java/de/struma/SuperDuperMarkt/controller/ImportController.java
+++ src/main/java/de/struma/SuperDuperMarkt/controller/ImportController.java
@@ -2,13 +2,14 @@ package de.struma.SuperDuperMarkt.controller;
 
 import de.struma.SuperDuperMarkt.model.Artikel;
 import de.struma.SuperDuperMarkt.service.ArtikelService;
+import de.struma.SuperDuperMarkt.service.CSVService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @RequestMapping(value = {"/import"})
@@ -16,7 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ImportController {
 
 	ArtikelService artikelService;
-	public ImportController(ArtikelService artikelService){this.artikelService = artikelService;}
+	CSVService csvService;
+	public ImportController(ArtikelService artikelService, CSVService csvService){
+		this.artikelService = artikelService;
+		this.csvService = csvService;
+	}
 
 	@GetMapping
 	public String showHome(Model model) {
@@ -28,6 +33,14 @@ public class ImportController {
 		artikelService.createNewArtikel(artikel);
 		model.addAttribute("newArtikel", new Artikel());
 		return "redirect:/import";
+	}
+	@PostMapping("/csv")
+	public String handleFileUpload(Model model,
+		   @RequestParam("file") MultipartFile file) throws IOException {
+		csvService.readFile(file);
+		model.addAttribute("message", "File uploaded successfully!");
+		return "pages/import";
+
 	}
 
 
